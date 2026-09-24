631f27bac26325d4c3498f0c4725beef33f776eb
INSHA-AHMED <inshaahmed2025@gmail.com>
2026-09-24 09:54:28 +0530

    Create solution.c

---
+++ 03-Time-Conversion/solution.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    char s[11];
+
+    scanf("%s", s);
+
+    int hour = (s[0] - '0') * 10 + (s[1] - '0');
+
+    if (s[8] == 'A')
+    {
+        if (hour == 12)
+            hour = 0;
+    }
+    else
+    {
+        if (hour != 12)
+            hour += 12;
+    }
+
+    printf("%02d%c%c%c%c%c%c\n",
+           hour,
+           s[2], s[3], s[4],
+           s[5], s[6], s[7]);
+
+    return 0;
+}
